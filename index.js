2e2f8045231a72f473a04639994776f421f00390
Stan <stanley.k.anderson@gmail.com>
2018-09-13 04:53:58 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+var url = require('url');
+var fs = require('fs');
+var http = require('http'),
+    httpProxy = require('http-proxy');
+
+
+httpProxy.createServer({
+  ssl: {
+    key: fs.readFileSync('mykey.key.pem', 'utf8'),
+    cert: fs.readFileSync('mykey.crt.pem', 'utf8')
+  },
+  target: 'http://localhost:8080',
+  secure: true // Depends on your needs, could be false.
+}).listen(8443);
+
+http.createServer(function (req, res) {
+  var pathname = url.parse(req.url).pathname;
+  res.writeHead(302, { 'Location': 'https://localhost:8443'+pathname });
+  res.end();
+}).listen(8000);
